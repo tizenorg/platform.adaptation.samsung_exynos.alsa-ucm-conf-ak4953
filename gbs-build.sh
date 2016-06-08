5e56bd968c360d9daae1550ae3fc17c33a32276d
wansu.yoo <wansu.yoo@samsung.com>
2016-06-08 14:27:13 +0900

    Add ucm conf for ak4953 codec.
    This patch add ucm conf files for ak4953 codec.
    
    Change-Id: I21c7af0bf08c0cf8d8cf46a3ca1fe1eb09886b59
    Signed-off-by: wansu.yoo <wansu.yoo@samsung.com>

---
+++ gbs-build.sh
@@ -0,0 +1,5 @@
+#rm /home/cwchoi00/GBS-ROOT/local/repos/spin/armv7l/RPMS/*.rpm
+
+gbs build -A armv7l --clean --include-all
+#gbs build -A aarch64 --clean --include-all
+#gbs build -A aarch64 --include-all
